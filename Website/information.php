717ee7db3b7f73386bd7a4b52db09b89750c95e9
TawsifKadir <118630521+TawsifKadir@users.noreply.github.com>
2023-10-22 14:37:29 +0600

    Add files via upload

---
+++ Website/information.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <style>
+        body {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+  background-color: #f0f0f0;
+}
+
+.dashboard-container {
+  max-width: 800px;
+  margin: 50px auto;
+  padding: 20px;
+  background-color: #fff;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+h1 {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+.student-card {
+  padding: 20px;
+  margin-bottom: 20px;
+  background-color: #f9f9f9;
+  border-radius: 5px;
+}
+
+.student-card h2 {
+  margin-bottom: 10px;
+}
+
+.student-card ul {
+  list-style: none;
+  padding: 0;
+}
+
+.student-card ul li {
+  margin-bottom: 5px;
+}
+
+.student-card ul li strong {
+  margin-right: 5px;
+}
+
+    </style>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Student Management Dashboard</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+<div class="dashboard-container">
+  <h1>Welcome to the Student Management Dashboard</h1>
+
+  <div class="student-card">
+    <h2>Student Information</h2>
+    <ul>
+      <li><strong>Name:</strong> John Doe</li>
+      <li><strong>Age:</strong> 20</li>
+      <li><strong>Grade:</strong> A</li>
+    </ul>
+  </div>
+
+</div>
+
+</body>
+</html>
